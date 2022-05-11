91606d36740738ce5350feb52ff35d397bfd1bf6
Turk <ko.turk@rabobank.nl>
2022-05-12 00:27:11 +0200

    Added TEQNation project

---
+++ TEQNation/PaymentEngine/basic/src/main/java/nl/blue4it/basic/processor/FraudProcessor.java
@@ -0,0 +1,27 @@
+package nl.blue4it.basic.processor;
+
+import example.avro.Fraud;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class FraudProcessor {
+    private Fraud fraud;
+
+    // 3.3 create kafka listener topics blacklists, and groupID something
+    public void consume(ConsumerRecord<?, ?> consumerRecord) {
+        log.info("received payload='{}'", consumerRecord.value());
+        // 3.4 cast to fraud
+    }
+
+    public Fraud getFraud() {
+        return fraud;
+    }
+
+    private void setFraud(Fraud fraud) {
+        this.fraud = fraud;
+    }
+}
