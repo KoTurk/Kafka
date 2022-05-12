91d9afcefa4045f41e6d01e852e2fb07fa22c232
Turk <ko.turk@rabobank.nl>
2022-05-12 15:29:05 +0200

    Added TEQNation conference code.

--- TEQNation/PaymentEngine/basic/src/main/java/nl/blue4it/basic/controller/PaymentController.java
+++ TEQNation/PaymentEngine/basic/src/main/java/nl/blue4it/basic/controller/PaymentController.java
@@ -27,6 +27,13 @@ public class PaymentController {
 
     private Payment createPayment() {
         // 2.1 create a payment
-        return null;
+        return Payment.newBuilder()
+                .setName("Mister Blue")
+                .setAmount(100.00F)
+                .setBalance(500.00F)
+                .setIban("NL61RABO0332546754")
+                .setToIban("NL61RABO0332543675")
+                .setProcessed(true)
+                .build();
     }
 }
